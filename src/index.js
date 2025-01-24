d3a3fd0a418016ea338748c071a1cfbba79b8349
Pratham Vaishnav <pratham@Prathams-MacBook-Air.local>
2025-01-24 22:32:36 +0530

    secound

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+
+
+
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!! ", err);
+// })
+
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express();
+
+
+(async () =>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("Connected to database");
+        app.on("error",(err)=>{
+            console.log("Error",err);
+            throw err
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("error",error);
+        throw error
+    }
+})()
+
+*/
+
