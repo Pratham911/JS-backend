de0a04603e05a150a548853b80c6bdb9c32b6358
Pratham Vaishnav <108391962+Pratham911@users.noreply.github.com>
2025-04-16 13:07:54 +0530

    utils code

--- Readme.md
+++ Readme.md
@@ -2,3 +2,14 @@
 
 <!-- how to make folder using terminal -->
 <!-- mkdir folder name -->
+<!-- how to add folder in github repositry
+1 git add.
+2 git commit -m "commit name like frist secound"
+3 git push -->
+
+ 
+<!-- utils means wreper -->
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config({
 
 
 
-connectDB()
+connectDB() 
 // .then(() => {
 //     app.listen(process.env.PORT || 8000, () => {
 //         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
@@ -26,7 +26,7 @@ connectDB()
 
 
 
-/*
+/* APPROCH 1 BY IIFA
 import express from "express";
 
 const app = express();
